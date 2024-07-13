5aced8c06c3f88f1f93865c3cf0cf3a7cf73a677
Soy Mentos <game.mendez447@gmail.com>
2024-07-12 19:55:31 -0600

    this

--- Punto.java
+++ Punto.java
@@ -1,15 +1,26 @@
 public class Punto {
     public int x;
     public int y;
+    public String nombre;
 
-    public Punto(int a) {
-        x = a;
-        y = a;
+    public Punto(int x) {
+        this.x = x;
+        this.y = x;
     }
 
-    public Punto(int a, int by) {
-        x = a;
-        y = by;
+    public Punto(int x, String nombre) {
+        this(x);
+        this.nombre = nombre;
+    }
+
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Punto(int x, int y, String nombre) {
+        this(x, y);
+        this.nombre = nombre;
     }
 
     public double calcularDistanciCentro() {
@@ -19,16 +30,15 @@ public class Punto {
     }
 
     // solo lo puedo invocar desde otro metodo propio de la clase
-    private void mostrarDatos() {
+    public void mostrarDatos() {
+        if (this.nombre == null)
+            System.out.println("El punto no tiene nombre, sus datos son:");
+        else
+            System.out.println("Punto con nombre: " + nombre);
         System.out.println("X: " + x);
         System.out.println("Y: " + y);
         System.out.println("La distacia con respecto al centro: " + calcularDistanciCentro());
         System.out.println("--------------");
-    }
-
-    public void mostrarDatos(String n) {
-        System.out.println(n);
-        mostrarDatos();
     }
 
 }
--- TesterPuntos.java
+++ TesterPuntos.java
@@ -2,13 +2,13 @@ public class TesterPuntos {
 
     public static void main(String[] args) {
 
-        Punto p1 = new Punto(2);
+        Punto p1 = new Punto(2, "alfa");
         Punto p2 = new Punto(1, 3);
         Punto p3 = new Punto(1);
 
-        p1.mostrarDatos("Punto p1");
-        p2.mostrarDatos("Punto p2");
-        p3.mostrarDatos("Punto p3");
+        p1.mostrarDatos();
+        p2.mostrarDatos();
+        p3.mostrarDatos();
     }
 
 }
